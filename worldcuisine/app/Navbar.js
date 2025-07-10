0ea43a34819ed5f09ba23d68bf62e2b1a8c52e86
Oskar Sadowski <oskarsadowski20@gmail.com>
2025-07-10 15:26:17 +0100

    Navbar template

---
+++ worldcuisine/app/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+
+<nav className="navbar">
+  
+  <div className="navbar-center">
+    <ul className="nav-links">
+      <li>
+        <a href="/">Discover</a>
+      </li>
+      <li>
+        <a href="/">Saved Dishes</a>
+      </li>
+      <li>
+        <a href="/
+        ">Randomizer</a>
+      </li>
+    </ul>
+  </div>
+  
+</nav>
+);
+};
+
+export default Navbar;
